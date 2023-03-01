169d4726bddcfc7225c83213b5a926ff54411b5d
Paul Cumming <user@users-MBP.broadband>
2023-03-01 20:44:18 +0000

    commiting labwork

--- functions_lab_1_start_point/src/python_functions_practice.py
+++ functions_lab_1_start_point/src/python_functions_practice.py
@@ -25,3 +25,7 @@ def add_string_as_number(string_1, string_2):
 def number_to_full_month_name(user_number):
     months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     return months[user_number - 1]
+
+def number_to_short_month_name(user_number):
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    return months[user_number - 1]
